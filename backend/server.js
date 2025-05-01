dac020a30b317709ad8a5e1d08633daa3b9a049a
khadija <khadija.khalid192@gmail.com>
2025-05-01 09:04:50 +0000

    updated port.env in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,20 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TODO',
-    console.log('MongoDB connected')
-)
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-app.listen(5000,
-    console.log('Server listening on port: 5000')
-)
+app.listen(process.env.PORT, () =>
+  console.log(`Server listening on port: ${process.env.PORT}`)
+);
 
 app.post('/add', (req, res) => {
   const { task } = req.body;
   TodoModel.create({ task })
       .then(result => res.json(result))
       .catch(err => console.log(err));
-   
+
 });
 
 app.get('/get',(req,res)=>{
@@ -28,7 +29,7 @@ app.get('/get',(req,res)=>{
   .then(result=> res.json(result))
   .catch(err=>console.log(err));
 });
-  
+ 
 app.put('/edit/:id',(req,res)=>{
   const{id} = req.params;
   TodoModel.findByIdAndUpdate(id,{done:true},{new:true})
@@ -49,6 +50,6 @@ app.delete('/delete/:id',(req,res)=>{
   TodoModel.findByIdAndDelete({_id:id})
   .then(result=> res.json(result))
   .catch(err=>res.json(err));
- }); 
+ }) 
 
 module.exports=app;
